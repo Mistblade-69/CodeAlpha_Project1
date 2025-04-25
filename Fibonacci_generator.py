3d17e9b50d1c5603b6ae704ccc02d1fde16c36ed
Mistblade-69 <sparktitan99@gmail.com>
2025-04-25 20:46:14 +0530

    Add files via upload

---
+++ Fibonacci_generator.py
@@ -0,0 +1,15 @@
+#This is a task for making of the fibonacci generator
+
+def fibonacci_generator(n):
+    a,b=0,1
+    print("The series is: ")
+    print(a,b,end=" ")
+    for i in range(n+1):
+        c=a+b
+        a=b
+        b=c
+        
+        print(c,end=" ")
+    return("\nThis is the fibonacci generator")
+#Example code
+print(fibonacci_generator(10))
